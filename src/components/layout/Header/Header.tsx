3ece44b80840b7d71f0d5ce287151b583a8daee6
Boris <102140528+blastuha@users.noreply.github.com>
2023-07-16 09:07:47 +0300

    16/07 without tailwind

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from 'react'
+import React from 'react'
 import { Logo } from '../../ui/Logo/Logo'
 import { HeaderNavList } from '../../ui/HeaderNavList/HeaderNavList'
 import { HeaderNavIcons } from '../../ui/HeaderNavIcons/HeaderNavIcons'
 
 import styles from './Header.module.scss'
 
-export const Header: FC = () => {
+export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <Logo />
