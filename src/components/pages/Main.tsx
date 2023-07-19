811bbba6393176e4cc38a6c3fb20197f112bb6f4
Boris <shevnin.boris2@yandex.ru>
2023-07-19 18:01:33 +0300

    19/07 18:01

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NewsSection } from '../ui/NewsSection'
+import MarkdownExample from '../ui/MarkdownExample'
 
 export const Main: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
@@ -7,6 +8,8 @@ export const Main: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <div className='container mx-auto pr-4 pl-4'>
         <div className='mb-14'>{children}</div>
         <NewsSection />
+        <MarkdownExample />
+        <h1>HELLOOOOOOOOOO</h1>
       </div>
     </main>
   )
