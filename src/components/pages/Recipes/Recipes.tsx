863258eeb9eb6f2f5dc9c277b1fc04f5bc92c1a8
Boris <shevnin.boris2@yandex.ru>
2023-08-09 13:57:13 +0300

    #8 RecipesPage edition

--- src/components/pages/Recipes/Recipes.tsx
+++ src/components/pages/Recipes/Recipes.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+
 import { Carousel } from './Carousel.tsx'
-import { Lines } from '../../ui/Lines.tsx'
+
+import { RecipeSection } from './RecipeSection.tsx'
+import { MostPopularCard } from './MostPopularCard.tsx'
 
 import { fetchReciepts } from '../../../api/fetchReciepts.ts'
-
 import { RecieptsData } from '../../../models/'
 
 export const Recipes: React.FC = () => {
@@ -18,46 +19,17 @@ export const Recipes: React.FC = () => {
 
   return (
     <div className='container mx-auto flex-grow'>
-      <div className='relative mb-10 mt-0'>
-        <Carousel />
-      </div>
-
-      <section className='grid h-fit grid-cols-3 gap-6'>
-        <div className='col-span-3 row-span-1'>
-          <h2 className='w-fit overflow-hidden whitespace-nowrap font-playfair text-[50px]'>
-            MOST POPULAR
-            <Lines firstLineHeight='h-[2px]' />
-          </h2>
-          <span className='font-raleway text-2xl leading-loose'>Must try dishes.</span>
-        </div>
-
-        <ul className='col-span-2 grid grid-cols-2 gap-6'>
-          {reciepts.map((recipe) => {
-            return (
-              // <Link to={`/reciept/${recipe.id}`} key={recipe.id}>
-              <div className='flex'>
-                <div className='group card w-96 cursor-pointer  rounded-lg bg-base-100'>
-                  <figure>
-                    <img
-                      src={recipe.photoURL}
-                      alt='picture'
-                      className='transform rounded-lg transition-transform duration-1000 group-hover:scale-110'
-                    />
-                  </figure>
-                  <div className='pb-4 pt-4'>
-                    <h2 className='card-title font-normal text-base-content transition-colors duration-1000 group-hover:underline'>
-                      {recipe.title}
-                    </h2>
-                  </div>
-                </div>
-              </div>
-              // </Link>
-            )
-          })}
-        </ul>
-
-        <div className='col-start-3 col-end-3'>Здесь может быть ваша реклама</div>
-      </section>
+      <Carousel blockStyles='relative mb-10 mt-0' />
+      <RecipeSection>
+        <>
+          <ul className='col-span-2 grid grid-cols-2 gap-6'>
+            {reciepts.map((recipe) => {
+              return <MostPopularCard {...recipe} />
+            })}
+          </ul>
+          <h2 className='col-start-3 col-end-3'>Здесь может быть ваша реклама</h2>
+        </>
+      </RecipeSection>
     </div>
   )
 }
